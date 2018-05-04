e013c807f0c6a795520b1468fffaa2f200ce4160
Black Phoenix <djunquero@edu.tecnocampus.cat>
2018-05-04 09:46:02 +0200

    SLIDE 16

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -10,7 +10,12 @@ import static org.junit.Assert.assertThat;
 
 public class BowlingGameTest {
 
+    @Test
     public void testGutterGame() throws Exception{
         Game g = new Game();
+
+        for(int i=0;i<20;i++){
+            g.roll(0);
+        }
     }
 }
