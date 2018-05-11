23d5f1b1cef5449c8eeda2a5227b34bfbf25dc72
David Junquero <djunquero@edu.tecnocampus.cat>
2018-05-11 14:10:11 +0200

    SLIDE 28

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -18,23 +18,20 @@ public class BowlingGameTest {
         g = new Game();
     }
 
-    @Test
-    public void testGutterGame() throws Exception{
-        int n = 20;
-        int pins = 0;
-        rollMany(n,pins);
-        assertEquals(0,g.score());
-    }
-
     private void rollMany(int n, int pins){
         for (int i = 0; i < n; i++)
             g.roll(pins);
     }
 
     @Test
+    public void testGutterGame() throws Exception{
+        rollMany(20,0);
+        assertEquals(0,g.score());
+    }
+
+    @Test
     public void testAllOnes() throws Exception{
-        for(int i = 0; i < 20; i++)
-            g.roll(1);
+        rollMany(0,1);
         assertEquals(20,g.score());
     }
 }
