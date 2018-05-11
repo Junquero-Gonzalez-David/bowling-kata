04aa48e0d0b23a21db40e98fa0a02aee1519774d
David Junquero <djunquero@edu.tecnocampus.cat>
2018-05-11 14:05:36 +0200

    SLIDE 23 corrected

--- src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
+++ src/test/java/com/drpicox/bowlingKata/BowlingGameTest.java
@@ -20,8 +20,6 @@ public class BowlingGameTest {
 
     @Test
     public void testGutterGame() throws Exception{
-        Game g = new Game();
-
         for(int i=0;i<20;i++){
             g.roll(0);
         }
@@ -30,7 +28,6 @@ public class BowlingGameTest {
 
     @Test
     public void testAllOnes() throws Exception{
-        Game g = new Game();
         for(int i = 0; i < 20; i++)
             g.roll(1);
         assertEquals(20,g.score());
